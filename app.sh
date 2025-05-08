9ed45b3da3702d545b5f479cb9989abe2ed7f378
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2025-05-08 16:16:20 +0800

    feat: add funcction to add expense

--- app.sh
+++ app.sh
@@ -26,6 +26,28 @@ add_income() {
     echo "Total Pendapatan: $total_income"
 }
 
+# Fungsi untuk menambahkan expense (pengeluaran)
+add_expense() {
+    total_expense=0
+    # Minta pengguna untuk memasukkan jumlah pengeluaran
+    echo "Masukkan jumlah pengeluaran: "
+    read amount
+    expense+=($amount) # Menambahkan pengeluaran ke dalam array expense
+    # Minta pengguna untuk memasukkan deskripsi pengeluaran
+    echo "Masukkan deskripsi pengeluaran: "
+    read desc
+    descriptions+=($desc) # Menambahkan deskripsi pengeluaran ke dalam array descriptions
+    echo "Pengeluaran berhasil ditambahkan."
+    # Menampilkan semua pengeluaran yang ada dan menghitung total pengeluaran
+    for ((i = 0; i < ${#expense[@]}; i++)); do
+        echo "Pengeluaran: ${expense[$i]} - Deskripsi: ${descriptions[$i + ${#income[@]}]}"
+        total_expense=$((total_expense + expense[$i])) # Menjumlahkan total pengeluaran
+    done
+    # Menampilkan hasil pengeluaran
+    echo "Total Pengeluaran: $total_expense"
+}
+
 while true; do
     add_income
+    add_expense
 done
